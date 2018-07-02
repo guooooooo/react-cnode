aa2b1542b7c01633f310a84f2a6c6e88a0a74e04
郭林浩 <gavin141113@163.com>
2018-07-02 20:51:00 +0800

    服务端渲染基本配置

--- server/server.js
+++ server/server.js
@@ -3,16 +3,23 @@ const ReactSSR = require('react-dom/server')
 const fs = require('fs')
 const path = require('path')
 
-const serverEntry = require('../dist/server-entry').default
-const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+const isDev = process.env.NODE_ENV === 'development'
+
 const app = express()
 
-app.use('/public', express.static(path.join(__dirname, '../dist')))
+if (!isDev) {
+    const serverEntry = require('../dist/server-entry').default
+    const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+    app.use('/public', express.static(path.join(__dirname, '../dist')))
 
-app.get('*', function(req, res){
-    const appString = ReactSSR.renderToString(serverEntry)
-    res.send(template.replace('<app></app>', appString))
-})
+    app.get('*', function(req, res){
+        const appString = ReactSSR.renderToString(serverEntry)
+        res.send(template.replace('<!--app-->', appString))
+    })
+} else {
+    const devStatic = require('./utils/dev-static')
+    devStatic(app)
+}
 
 app.listen(3333, function () {
     console.log('server is listening on port 3333')

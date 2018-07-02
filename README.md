5467596c900dcd5918a8d4202862dbd2790ad152
郭林浩 <gavin141113@163.com>
2018-07-02 18:35:51 +0800

    init工程基本配置

---
+++ README.md
@@ -0,0 +1,10 @@
+
+HtmlWebpackPlugin
+
+这个插件将生成一个HTML5文件，该文件的 body 中使用script 标记引用了所有 webpack bundle。
+
+react-dom/server
+
+服务端渲染 
+- 利于SEO
+- 提高首屏渲染的速度
---
+++ build/webpack.config.client.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+    mode: 'development',
+    entry: {
+        app: path.join(__dirname, '../client/app.js')
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.join(__dirname, '../dist'),
+        publicPath: '/public/'        
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '../client/template.html')
+        })
+    ]
+}
+
+if (isDev) {
+    config.entry = {
+        app: [
+            'react-hot-loader/patch',
+            path.join(__dirname, '../client/app.js')
+        ]
+    }
+    config.devServer = {
+        host: '0.0.0.0',
+        port: '8888',
+        contentBase: path.join(__dirname, '../dist'),
+        hot: true,
+        overlay: {
+            errors: true
+        },
+        publicPath: '/public',
+        historyApiFallback: {
+            index: '/public/index.html'
+        }
+    }
+    config.plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+
+module.exports = config
